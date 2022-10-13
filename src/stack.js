265fc00053364da482e0b84b3c7268c43df97d2e
DrobyshSV <austin@tut.by>
2022-10-13 18:34:20 +0300

    feat: add stack  js code

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    stack = [];
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+        this.stack.push(element);
+    }
+
+    pop() {
+        return this.stack.pop();
+    }
+
+    peek() {
+        return  this.stack[this.stack.length - 1];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack
 };
